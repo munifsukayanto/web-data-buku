c18f229448f9fc1a82a937cb337a89a659bf9523
munifsukayanto <157446520+munifsukayanto@users.noreply.github.com>
2024-01-23 13:34:15 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrasi</title>
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header>
+    
+        <h1>DATA BUKU</h1>
+       <hr>
+    <br>
+    </header>
+        <form action="login.php"></form>
+    <center>
+       <input type="text" placeholder="Search.." name="search" class="fill">
+        <button type="submit"><i class="fa fa-search"></i></button>
+    </center>
+
+    <div class="gallery">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhKa8BAwYbBMgZP3YwyUKYX6TTodhz-yJ1kA&usqp=CAU" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgYFkg9GWBdu6PpYbAbMyKcxE3nVNFvIL0qA&usqp=CAU" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS5RlB69kc6FY2EnpcISqXQ_qmwHj2-cnpAw&usqp=CAU" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRArjjgED1IGRK4ROHEiCYU89DfC_XJACQ-2jPzs-VFAGEMYpTnjINl8fX1-GWQJ09VKcU&usqp=CAU" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb8568OzEKhVBo-udNJprrJFKYZgOyEUu0Pw&usqp=CAU" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtckubVLYJEboW-
+                    MYi1I9NB1hFazC66qQ7JYwDBq6danbnNSnSyPAMlfqaHD-nTNL8z2g&usqp=CAU" alt="">
+    
+    </div>
+    
+</body>
+</html>
